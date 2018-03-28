8d2a50f4e26ace71aed3d86dfdba853e631c19f5
garrenhendricks <37517416+garrenhendricks@users.noreply.github.com>
2018-03-28 12:38:20 -0700

    Update radar_publisher.cpp

--- src/radar_publisher.cpp
+++ src/radar_publisher.cpp
@@ -147,20 +147,26 @@ void process_json(OPS241A_radar::radar_data *data, std::string single_msg)
 //#########################################################################################################################################################//
 //#########################################################################################################################################################//
 
-std::string getMessage(CommConnection *connection) {
+std::string getMessage(CommConnection *connection) 
+{
 	std::string msg;
 	bool startFilling = false; 
-	while(true) {
-		if(connection->available()) {
+	while(true) 
+	{
+		if(connection->available()) 
+		{
 			char c = connection->read();
-			if(c == '{') {
+			if(c == '{') 
+			{
 				startFilling = true;
-			} else if(c == '}' && startFilling) {
+			}
+			else if(c == '}' && startFilling) 
+			{
 				msg += c;
-			//	ROS_INFO("msg = %s", msg.c_str());
 				return msg;
 			}
-			if(startFilling) {
+			if(startFilling)
+			{
 				msg += c;
 			}
 		}
@@ -202,140 +208,29 @@ int main(int argc, char** argv)
   connection.write("Of"); 
   connection.clearBuffer();
   // contines the while loop as long as ros::ok continues to continue true
-  while (ros::ok()) 
+  while (ros::ok())
   {
   	OPS241A_radar::radar_data info; 
-	  OPS241A_radar::radar_data info_out;
-  	expected_msgs = (int)fft_on+1;
-  	//ROS_INFO("Expected Msgs: %i",expected_msgs);
-  	 	
+	OPS241A_radar::radar_data info_out;
+  	//creats an instant of the radar_data structure named info. format: 
+	//package_name::msg_file_name variable_instance_name; 	
   	std::string msg = getMessage(&connection);
   	std::string msg_fft = getMessage(&connection);
 
- 		if (msg.empty())
- 		{
- 			//ROS_INFO("Entered empty msg");
- 			ros::spinOnce();
- 			loop_rate.sleep();
- 			if (out_ready)
-			{ 
-				//broadcast the message to any subscriber
-				//radar_pub.publish(info_out); 
-			}
-			radar_pub.publish(info_out);
- 			continue;
- 		}
- 		process_json(&info, msg);
- 		//ROS_INFO("info returned");
- 		process_json(&info, msg_fft);
- 		//ROS_INFO("info returned again");
- 		//ROS_INFO("fft: %s",msg_fft);
- 		/*
- 		else
- 		{  		
- 			std::size_t found_open_brace = msg.find("{");
- 			int found_close_brace = needToRead-1;
- 			//std::size_t found_close_brace = msg.find("}");
- 		  bool open = (found_open_brace != std::string::npos);
- 		  bool close = (found_close_brace != std::string::npos);
- 		  bool greater = (found_open_brace < found_close_brace);
-
- 			if ((open) && (close) && (greater))
- 				{
- 				  ROS_INFO("Case 1");
- 				 //Process message and populate.
-					std::string whole_msg = msg.substr(found_open_brace,found_close_brace+1);
-					info = process_json(info, whole_msg);
-					num_processed = num_processed+1;
-					
-					//Catch the start to the second message if it exists.
-					std::string start_second_msg = msg.substr(found_close_brace+2, msg.size());
-					std::size_t second_open_brace = start_second_msg.find("{");
-					if ( second_open_brace != std::string::npos)
-					{
-						build_data.clear();
-						build_data = start_second_msg.substr(second_open_brace, start_second_msg.size());
-					}
- 				}
- 			else if ((open) && !(close))
- 				{
- 				  //Make a substring from the open brace position to the end of msg and set the build data to the substring. not processing
- 					ROS_INFO("Case 2");
- 					build_data.clear();
- 					build_data = build_data + msg.substr(found_open_brace, msg.size());
- 					
- 				}
- 			else if (!(open) && !(close))
- 				{
- 					ROS_INFO("Case 3");
- 					//append new information/data. Not processing
- 					build_data = build_data + msg;
- 				}
- 			else if (!(open) && (close))
- 				{
- 					ROS_INFO("Case 4");
- 					//appending the last of the message 
- 					build_data = build_data + msg.substr(0,found_close_brace+1);
- 					info = process_json(info, build_data);
- 					num_processed = num_processed+1;
- 					//for data after the close brace
- 					build_data.clear();
- 				}
- 			//case where the end of a current message is found and the start of the next one - need to check
- 			//within the 2nd message to determine whether or not a full message has been found.
- 			else if ((open) && (close))
- 			{
- 					ROS_INFO("Case 5");
- 					ROS_INFO("Location of open brace: %i", open);
- 			 		build_data = build_data + msg.substr(0,found_close_brace+1);
- 					info = process_json(info, build_data);
- 					num_processed = num_processed+1;
- 					//find the very last close brace of the msg. Could be part of 1st and only msg, maybe part of a second complete msg.
- 					std::size_t last_close_brace = msg.find_last_of("}");
- 					//check to see if the very last close brace is the same as the close brace of the first msg. It would this is the only msg.
- 					bool same_brace = last_close_brace == found_close_brace;
- 					
- 					if (same_brace) //there is .......}{....... there MAY BE a second message that is not complete
- 					{
- 						ROS_INFO("Case 6");
- 						//create a string for the second msg. If there is no second msg then this will be empty
-	 					std::string second_msg = msg.substr(found_open_brace,msg.size());
-	 					build_data.clear();
-	 					build_data = second_msg;
-					}
-					//when last_close_brace != found_close_brace }{} there is another complete msg we need to process
-					else 
-					{
-						ROS_INFO("Case 7");
-						std::string second_msg = msg.substr(found_open_brace,last_close_brace);
- 					  info = process_json(info, second_msg);
- 					  num_processed = num_processed+1;
-					}
- 			}
- 			else
- 				{
- 				 ROS_ERROR("Code should never reach this point");
- 				}
- 			*/	
- 		if (num_processed >= expected_msgs)
-		{
-			num_processed = 0;
-			out_ready = 1;
-			info_out = info;
-		}
-		/*
-		if (out_ready)
-		{ 
-			//broadcast the message to any subscriber
-			radar_pub.publish(info); 
-		}
-		*/
-		radar_pub.publish(info);
-		//becomes neccessary for subscriber callback functions
-		ros::spinOnce(); 
-		// forces loop to wait for the remaining loop time to finish before starting over
-		loop_rate.sleep(); 
-		//}
+ 	if (msg.empty())
+	{
+ 		ros::spinOnce();
+ 		loop_rate.sleep();
+		radar_pub.publish(info_out);
+ 		continue;
+ 	}
+ 	process_json(&info, msg);
+ 	process_json(&info, msg_fft);	
+	radar_pub.publish(info);
+	//becomes neccessary for subscriber callback functions
+	ros::spinOnce(); 
+	// forces loop to wait for the remaining loop time to finish before starting over
+	loop_rate.sleep();
   }
   
   return(0);
